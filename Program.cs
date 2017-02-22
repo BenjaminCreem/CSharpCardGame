1e6e08ae4dad2b1096471498a6f8059978ca45a5
BenjaminCreem <benjamincreem@gmail.com>
2017-02-22 11:08:50 -0500

    Added basic card class. May still have more things to add to it such as other ways to compare

--- Program.cs
+++ Program.cs
@@ -2,11 +2,50 @@
 
 namespace CardGame
 {
+	class Card
+	{
+		private string suit;
+		private int value; //Jack = 11, Queen = 12, King = 13, Ace = 14
+		private Card(string s, int v)
+		{
+			suit = s;
+			value = v;
+		}
+		public Boolean valueEquals(Card c)
+		{
+			if (value == c.getValue())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		public string getSuit()
+		{
+			return suit;
+		}
+		public int getValue()
+		{
+			return value;
+		}
+		public void setSuit(string s)
+		{
+			suit = s;
+		}
+		public void setValue(int v)
+		{
+			value = v;
+		}
+	}
+
+
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			
 		}
 	}
 }
