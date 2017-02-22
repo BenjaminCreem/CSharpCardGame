aed95d0c8ee76d68563b5f68bcbaf2e9b3bbb403
BenjaminCreem <benjamincreem@gmail.com>
2017-02-22 11:27:48 -0500

    Added basic deck. Added ToString for card. Added test method for creating a basic deck

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CardGame
 {
+	//suit can either be club, spade, heart, or diamond
+	//value is an integer from 2 to 14 inclusive
+	//this is a basic card class. suit does not matter for go fish, but is included anyway
+	//for the sake of simulating a real card and for potential future use
 	class Card
 	{
 		private string suit;
 		private int value; //Jack = 11, Queen = 12, King = 13, Ace = 14
-		private Card(string s, int v)
+		public Card(string s, int v)
 		{
 			suit = s;
 			value = v;
@@ -38,14 +43,45 @@ namespace CardGame
 		{
 			value = v;
 		}
+		public override string ToString()
+		{
+			return value + " of " + suit;
+		}
 	}
 
+	class Deck
+	{
+		List<Card> group = new List<Card>();
+		//A basic deck will have 52 cards, 4 of each suit. 
+		public Deck()
+		{
+			for (int i = 2; i <= 14; i++)
+			{
+				group.Add(new Card("clubs", i));
+				group.Add(new Card("diamonds", i));
+				group.Add(new Card("hearts", i));
+				group.Add(new Card("spades", i));
+			}
+		}
+
+
+
+		//Method is dedicated to testing if creating a deck works 
+		public void TestDeckConstruct()
+		{
+			for (int i = 0; i < group.Count; i++)
+			{
+				Console.WriteLine(group[i]);
+			}
+		}
+	}
 
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			
+			Deck deck = new Deck();
+			deck.TestDeckConstruct();
 		}
 	}
 }
